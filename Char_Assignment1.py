fecf948cee55325e4fa912ad1831b19dea50695c
Ian Char <ianchar4@gmail.com>
2015-08-25 13:30:59 -0600

    Completed tree

--- Char_Assignment1.py
+++ Char_Assignment1.py
@@ -44,7 +44,7 @@ class Stack(object):
 class Node(object):
     def __init__(self, key, parent = None, leftChild = None, rightChild = None):
         self.key = key
-        self.parent = key
+        self.parent = parent
         self.leftChild = leftChild
         self.rightChild = rightChild
 
@@ -67,34 +67,92 @@ class Node(object):
             raise TypeError("Nodes can only have integers for keys.")
 
     def setParent(self, parentNode):
-        if isinstance(parentNode, Node):
+        if isinstance(parentNode, Node) or parentNode is None:
             self.parent = parentNode
         else:
-            raise TypeError("Parent must be of type Node.")
+            raise TypeError("Parent must be of type Node or None.")
 
     def setLeftChild(self, leftChild):
-        if isinstance(leftChild, Node):
+        if isinstance(leftChild, Node) or leftChild is None:
             self.leftChild = leftChild
         else:
-            raise TypeError("Children must be of type Node.")
+            raise TypeError("Children must be of type Node or None.")
 
     def setRightChild(self, rightChild):
-        if isinstance(rightChild, Node):
+        if isinstance(rightChild, Node) or rightChild is None:
             self.rightChild = rightChild
         else:
-            raise TypeError("Children must be of type Node.")
+            raise TypeError("Children must be of type Node or None.")
+
+    def __str__(self):
+        return str(self.key)
 
 class BinaryTree(object):
     def __init__(self, rootValue):
         self.root = Node(rootValue)
-        self.activeNodes = {self.root: True}
+        self.keyNodeMap = {rootValue: [self.root]}
 
     def getRoot(self):
         return self.root
-        
-    def add(value, parentValue)
-        try:
 
+    # Will try to add to the oldest node with parentValue first.
+    # This will add one node to the tree at most.
+    def add(self, value, parentValue):
+        if not parentValue in self.keyNodeMap:
+            print "Parent not found."
+            return
+        parentList = self.keyNodeMap[parentValue]
+        nodeHasBeenAdded = False
+        for n in parentList:
+            if n.getLeftChild() is None:
+                newNode = Node(value, n)
+                n.setLeftChild(newNode)
+                if not value in self.keyNodeMap:
+                    self.keyNodeMap[value] = [newNode]
+                else:
+                    self.keyNodeMap[value].append(newNode)
+                nodeHasBeenAdded = True
+                break
+            elif n.getRightChild() is None:
+                newNode = Node(value, n)
+                n.setRightChild(newNode)
+                if not value in self.keyNodeMap:
+                    self.keyNodeMap[value] = [newNode]
+                else:
+                    self.keyNodeMap[value].append(newNode)
+                nodeHasBeenAdded = True
+                break
+        if not nodeHasBeenAdded:
+            print "Parent(s) has two children, node not added."
+
+    # Will delete all valid nodes with the given value
+    def delete(self, value):
+        if not value in self.keyNodeMap:
+            print "Node not found."
+            return
+        nodeList = self.keyNodeMap[value]
+        for n in nodeList:
+            if n.getLeftChild() is None and n.getRightChild() is None:
+                parent = n.getParent()
+                if parent.getLeftChild() is n:
+                    parent.setLeftChild(None)
+                else:
+                    parent.setRightChild(None)
+                nodeList.remove(n)
+            else:
+                print "Node not deleted, has children."
+        if len(nodeList) == 0:
+            del self.keyNodeMap[value]
+
+    # Returns a string. Will be called in the context: print <someTreeHere>
+    def __str__(self):
+        def printCurrNode(currNode):
+            if currNode is None:
+                return ""
+            return (" " + str(currNode) + " "
+                    + printCurrNode(currNode.getLeftChild())
+                    + printCurrNode(currNode.getRightChild()))
+        return printCurrNode(self.root)
 
 # 5. Testing
 import random
@@ -123,4 +181,23 @@ def testStrucs():
     for _ in range(10):
         print "Popped", stack.pop(), " Current size:", stack.checkSize()
 
-testStrucs()
+    print "\n-----------------3 Tree-----------------"
+    print "Building tree... "
+    tree = BinaryTree(0)
+    tree.add(1, 0)
+    tree.add(2, 0)
+    tree.add(3, 1)
+    tree.add(4, 1)
+    tree.add(5, 2)
+    tree.add(6, 2)
+    tree.add(7, 3)
+    tree.add(8, 3)
+    tree.add(9, 4)
+    print tree
+    print "\nRemoving 6 and 7..."
+    tree.delete(6)
+    tree.delete(7)
+    print tree
+
+if __name__ == "__main__":
+    testStrucs()
